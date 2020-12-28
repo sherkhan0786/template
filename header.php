d4e4c88e2ec0266b0bbd78d406e91d66098cd063
Sher khan <52857625+sherkhan0786@users.noreply.github.com>
2020-12-28 20:36:35 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img class="logo" src="img/logo.png" alt="logo" height="50"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav float-md-left">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Form</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
---
+++ products.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Products
+	</title>
+	<!-- <link href="style.css" type="text/css" rel="stylesheet"> -->
+</head>
+<body>
+	<div id="header">
+		<?php include 'header.php'; ?>
+	</div>
+	<div id="main">
+		<div id="products">
+			<div id="product-101" class="product">
+				<img src="img/football.png">
+				<h3 class="title"><a href="#">Product 101</a></h3>
+				<span>Price: $150.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+			<div id="product-101" class="product">
+				<img src="img/tennis.png">
+				<h3 class="title"><a href="#">Product 102</a></h3>
+				<span>Price: $120.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+			<div id="product-101" class="product">
+				<img src="img/basketball.png">
+				<h3 class="title"><a href="#">Product 103</a></h3>
+				<span>Price: $90.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+			<div id="product-101" class="product">
+				<img src="img/table-tennis.png">
+				<h3 class="title"><a href="#">Product 104</a></h3>
+				<span>Price: $110.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+			<div id="product-101" class="product">
+				<img src="img/soccer.png">
+				<h3 class="title"><a href="#">Product 105</a></h3>
+				<span>Price: $80.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<nav>
+			<ul id="footer-links">
+				<li><a href="#">Privacy</a></li>
+				<li><a href="#">Declaimers</a></li>
+			</ul>
+		</nav>
+	</div>
+</body>
+</html>
